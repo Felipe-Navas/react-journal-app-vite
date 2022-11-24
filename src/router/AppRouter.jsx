d94f12fb9a71251ae92a58c39529f9d9469ab5da
Felipe Navas <felipenavas.itec@gmail.com>
2022-11-24 08:45:07 -0300

    Adding router-dom and some pages

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,13 @@
+import { Routes } from "react-router-dom"
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      {/* Login and register */}
+      <Route />
+
+      {/* JournalApp */}
+      <Route />
+    </Routes>
+  )
+}
