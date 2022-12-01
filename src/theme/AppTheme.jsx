7b841083778ffbd1234614e24d1ed9a6269e56c2
Felipe Navas <felipenavas.itec@gmail.com>
2022-12-01 09:06:33 -0300

    Theme aplied

--- src/theme/AppTheme.jsx
+++ src/theme/AppTheme.jsx
@@ -1,9 +1,10 @@
 import { ThemeProvider } from '@emotion/react'
 import { CssBaseline } from '@mui/material'
+import { purpleTheme } from './'
 
 export const AppTheme = ({ children }) => {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={purpleTheme}>
       <CssBaseline />
       {children}
     </ThemeProvider>
